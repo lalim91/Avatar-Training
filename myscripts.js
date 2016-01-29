c7080886dffd2f133cdeb6bc13a269b0bba165bf
Leslie Ann Lim <Lalim@Leslies-MacBook-Air.local>
2016-01-29 10:50:22 -0800

    made the cards clickable

--- myscripts.js
+++ myscripts.js
@@ -5,27 +5,37 @@ var match_counter = 0;
 
 
 $(document).ready(function(){
-    $('.back').click(card_clicked);
+ /*I tried making a global var for the class "back" (var card = $('.body'))
+   but the substitution wouldn't happen*/
+    $('.card').click(function(){
+        card_clicked(this);
+    });
 });
 
 
 
-function card_clicked() {
-   var card_flipped = $(this).hide();
+function card_clicked(element) {
+    $(element).find('.back').hide();
+}
 
-    if (first_card_clicked != null) {
+
+  /* if (first_card_clicked != null) {
         first_card_clicked = card_flipped;
     } else {
         second_card_clicked = card_flipped;
         if(first_card_clicked == second_card_clicked){
-            match_counter += 1;
+            m  atch_counter += 1;
             first_card_clicked = null;
             second_card_clicked = null;
             if (match_counter == total_possible_matches) {
                 console.log("Yay! You won!");
             }
         }else {
+            setTimeout(flip_back, 2000);
+            function flip_back () {
+                $('.back').show();
+            }
 
         }
     }
-}
+}*/
