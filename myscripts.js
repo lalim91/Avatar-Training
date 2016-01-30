21f6535e2757e294eb697594c2bf893a072ccdb9
Leslie Ann Lim <Lalim@Leslies-MacBook-Air.local>
2016-01-29 17:55:20 -0800

    test

--- myscripts.js
+++ myscripts.js
@@ -5,8 +5,8 @@ var match_counter = 0;
 
 
 $(document).ready(function(){
- /*I tried making a global var for the class "back" (var card = $('.body'))
-   but the substitution wouldn't happen*/
+    /*I tried making a global var for the class "back" (var card = $('.body'))
+     but the substitution wouldn't happen*/
     $('.card').click(function(){
         card_clicked(this);
     });
@@ -15,16 +15,16 @@ $(document).ready(function(){
 
 
 function card_clicked(element) {
-    $(element).find('.back').hide();
-}
+    var card_flipped= $(element).find('.back').hide();
 
 
-  /* if (first_card_clicked != null) {
+
+    /*if (first_card_clicked != null) {
         first_card_clicked = card_flipped;
     } else {
         second_card_clicked = card_flipped;
         if(first_card_clicked == second_card_clicked){
-            m  atch_counter += 1;
+            match_counter += 1;
             first_card_clicked = null;
             second_card_clicked = null;
             if (match_counter == total_possible_matches) {
@@ -37,5 +37,5 @@ function card_clicked(element) {
             }
 
         }
-    }
-}*/
+    }*/
+}
