cf0dca547c9313139ad37dba8a934e7bfff7d5e7
Leslie Ann Lim <Lalim@Leslies-MacBook-Air.local>
2016-03-31 09:52:24 -0700

    added face generators #1

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
  */
 var gameController = function (element){
     var self = this;
-    self.element = element;
+    self.element = $(element);
     var cards = [];
     this.createCards = function(){
         var newCard = new cardGenerator();
@@ -11,11 +11,39 @@ var gameController = function (element){
     };
     var cardGenerator = function(){
         var cgSelf = this;
+        cgSelf.cardContainer = null;
         cgSelf.cardElement = null;
+
         this.renderCard = function(){
-            cgSelf.cardElement = $('<div>').addClass('card');
-            return cgSelf.cardElement;
+            var divCon = $('<div>');
+            var divCard = $('<div>');
+            cgSelf.cardElement = divCard.addClass('card');
+            cgSelf.cardContainer = divCon.addClass('container');
+            cgSelf.cardContainer.append(cg.cardElement);
+            return cgSelf.cardContainer;
         }
+    };
+
+    var backGenerator = function (){
+        var bgSelf = this;
+        var backImgSrc = "image/airSymbol.png";
+        bgSelf.backCard = null;
+        this.renderBackCard = function (){
+            var img = $('<img>');
+            bgSelf.backCardImg = img.addClass('cardImg').attr('src',backImgSrc);
+            return bgSelf.backCardImg;
+        }
+
+    };
+    var frontGenerator = function (frontData){
+        var fgSelf = this;
+        fgSelf.frontCard = null;
+        this.renderFrontCard = function (){
+            var img = $('<img>');
+            fgSelf.frontCard = img.addClass('cardImg '+ 'front');
+            return fgSelf.frontCard;
+        }
+
     };
     var statGenerator = function (){
         var sgSelf = this;
