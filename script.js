d837380bad9994f9a9488bbe11284bceb7880af2
Leslie Ann Lim <Lalim@Leslies-MacBook-Air.local>
2016-03-30 18:49:19 -0700

    added game generator and stats generator

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 /**
  * Created by Lalim on 3/30/16.
  */
+var gameController = function (element){
+    var self = this;
+    self.element = element;
+    var cards = [];
+    this.createCards = function(){
+        var newCard = new cardGenerator();
+        cards.push(newCard);
+    };
+    var cardGenerator = function(){
+        var cgSelf = this;
+        cgSelf.cardElement = null;
+        this.renderCard = function(){
+            cgSelf.cardElement = $('<div>').addClass('card');
+            return cgSelf.cardElement;
+        }
+    };
+    var statGenerator = function (){
+        var sgSelf = this;
 
-var cardGenerator = function() {
-
+    }
 };
+
+
+
+
