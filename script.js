a6c56474a3395d96d13c677cf6402bb05a9440fc
Leslie Ann Lim <Lalim@Leslies-MacBook-Air.local>
2016-04-01 02:03:15 -0700

    trying to add img to correct side

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ var gameController = function (element){
     var self = this;
     self.element = $(element);
     var cards = [];
+    var frontObj = [];
+    var frontImg = [];
+    var randomFront = [];
     this.createCards = function(height,width){
         for (var i = 0; i < height; i++) {
             var tr = $('<tr>');
@@ -17,8 +20,45 @@ var gameController = function (element){
             self.element.append(tr);
         }
     };
-    this.createFaces = function(){
+    this.createFront = function(frontData){
+        var newFront = null;
+        var frontElement = null;
+        for(var i = 0; i < frontData.length; i++){
+            for(var j = 1;j < cards.length && frontData[i].frontCount > 0; j++){
+                frontData[i].frontCount--;
+                newFront = new frontGenerator();
+                frontElement = newFront.renderFrontCard();
+                frontElement.attr('src',frontData[i].frontSrc);
+                frontObj.push(newFront);
+                frontImg.push(frontElement);
+            }
+        }
+        console.log('frontObj array contains', frontObj);
+        console.log('frontImg array contains', frontImg);
+    };
+    this.shuffleDeck = function(){
+        randomFront = frontImg.slice(0);
+        for (var i = randomFront.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = randomFront[i];
+            randomFront[i] = randomFront[j];
+            randomFront[j] = temp;
+        }
+        console.log(randomFront);
+        return randomFront;
+    };
+    this.totalCard = function (){
+        var newback = null;
+        var backElement = null;
+        for(var i = 0; i < cards.length; i++){
+            for (var j = 0; j < randomFront.length; j++){
+                newback = new backGenerator();
+                backElement = newback.renderBackCard();
+            }
+            cards[i].cardElement.append(randomFront[j]);
+            cards[i].cardElement.append(backElement);
 
+        }
     };
 
     var cardGenerator = function(){
@@ -47,7 +87,7 @@ var gameController = function (element){
         }
 
     };
-    var frontGenerator = function (frontData){
+    var frontGenerator = function (){
         var fgSelf = this;
         fgSelf.frontCard = null;
         this.renderFrontCard = function (){
@@ -63,12 +103,53 @@ var gameController = function (element){
     }
 };
 
+var frontData = [
+    {
+        frontSrc:'image/Uncle_Iroh.png',
+        frontCount:2
+    },
+    {
+        frontSrc:'image/toph.png',
+        frontCount:2
+    },
+    {
+        frontSrc:'image/sokka.png',
+        frontCount:2
+    },
+    {
+        frontSrc:'image/Momo.png',
+        frontCount:2
+    },
+    {
+        frontSrc:'image/Katara.png',
+        frontCount:2
+    },
+    {
+        frontSrc:'image/bumi.png',
+        frontCount:2
+    },
+    {
+        frontSrc:'image/appa.png',
+        frontCount:2
+    },
+    {
+        frontSrc:'image/aang.png',
+        frontCount:2
+    },
+    {
+        frontSrc:'image/avatar.png',
+        frontCount:2
+    }
+];
+
 
 var game;
 $(document).ready(function(){
     game = new gameController('#gameDiv');
     game.createCards(3,6);
+    game.createFront(frontData);
+    game.shuffleDeck();
+    game.totalCard();
+
 
 });
-
-
